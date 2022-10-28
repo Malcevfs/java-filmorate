d2145e11c8c5b8ad5b8737d171ad8c3b82c084e9
Fedor Malcev <mallcevfs@yandex.ru>
2022-10-28 10:15:34 +0300

    Проект 10 спринта. Добавлены новые эндпоинты и проработан класс Service.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.controller;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.yandex.practicum.filmorate.model.Film;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storrage.film.FilmStorage;
-
 import javax.validation.Valid;
 import java.util.Collection;
 import java.util.List;
@@ -15,43 +13,35 @@ import java.util.List;
 public class FilmController {
     private final FilmStorage filmStorage;
     private final FilmService filmService;
-
     @Autowired
     public FilmController(FilmStorage filmStorage, FilmService filmService) {
         this.filmStorage = filmStorage;
         this.filmService = filmService;
     }
-
     @PostMapping
     public Film add(@Valid @RequestBody Film film) {
         return filmStorage.add(film);
     }
-
     @PutMapping
     public Film refresh(@Valid @RequestBody Film film) {
         return filmStorage.refresh(film);
     }
-
     @GetMapping
     public Collection<Film> getAll() {
         return filmStorage.getAll();
     }
-
     @GetMapping("/{id}")
     public Film getFilmById(@PathVariable("id") Integer id) {
         return filmStorage.getFilmById(id);
     }
-
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable("id") Integer id, @PathVariable("userId") Integer userId) {
         filmService.addLike(id, userId);
     }
-
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLike(@PathVariable("id") Integer id, @PathVariable("userId") Integer userId) {
         filmService.deleteLike(id, userId);
     }
-
     @GetMapping("/popular")
     public List<Film> getTopFilm(
             @RequestParam(value = "count", defaultValue = "10", required = false) Integer count) {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -16,50 +16,41 @@ import java.util.List;
 public class UserController {
     private final UserStorage userStorage;
     private final UserService userService;
-
     @Autowired
     public UserController(UserStorage userStorage, UserService userService) {
         this.userStorage = userStorage;
         this.userService = userService;
     }
-
     @PostMapping
     public User add(@Valid @RequestBody User user) {
 
         return userStorage.add(user);
     }
-
     @PutMapping
     public User refresh(@Valid @RequestBody User user) {
 
         return userStorage.refresh(user);
     }
-
     @GetMapping
     public Collection<User> getAll() {
         return userStorage.getAll();
     }
-
     @GetMapping("/{id}")
     public User getFilmById(@PathVariable("id") Integer id) {
         return userStorage.getUserById(id);
     }
-
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
         userService.addFriend(id, friendId);
     }
-
     @DeleteMapping("{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable("id") Integer id, @PathVariable("friendId") Integer friendId) {
         userService.deleteFriend(id, friendId);
     }
-
     @GetMapping("/{id}/friends")
     public List<User> getFriendsList(@PathVariable("id") Integer id) {
         return userService.getAllFriends(id);
     }
-
     @GetMapping("/{id}/friends/common/{otherId}")
     public List<User> getOtherFriendsList(@PathVariable("id") Integer id, @PathVariable("otherId") Integer otherId) {
         return userService.getOtherFriends(id, otherId);
